6d3b3fee617588b8a66940166a8a02bfaa716168
CDE Test <cde_test@example.com>
2022-11-26 22:39:48 +0700

    Init

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
 echo "===== Upload docker ====="
+echo "Docker user: $DOCKER_USER"
 
-# Step 1:
-# Create dockerimage
-dockerimage="my-project5-app"
+# # Step 1:
+# # Create dockerimage
+# dockerimage="my-project5-app"
 
-# Step 2:
-# Build image and add a descriptive tag
-docker build --tag $dockerimage .
+# # Step 2:
+# # Build image and add a descriptive tag
+# docker build --tag $dockerimage .
 
-# Step 3: 
-# List docker images
-docker image ls
+# # Step 3: 
+# # List docker images
+# docker image ls
 
-# Step 4:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
-dockerpath="sunvndev/$dockerimage"
+# # Step 4:
+# # Create dockerpath
+# # dockerpath=<your docker ID/path>
+# dockerpath="sunvndev/$dockerimage"
 
-# Step 5:  
-# Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
-docker tag $dockerimage $dockerpath
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+# # Step 5:  
+# # Authenticate & tag
+# echo "Docker ID and Image: $dockerpath"
+# docker tag $dockerimage $dockerpath
+# docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-# Step 6:
-# Push image to a docker repository
-docker push $dockerpath
+# # Step 6:
+# # Push image to a docker repository
+# docker push $dockerpath
 
 
