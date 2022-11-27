81b743c8c811c502d2096ffb712ede2e9cff7337
CDE Test <cde_test@example.com>
2022-11-27 22:53:00 +0700

    Update config.yml

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -2,9 +2,8 @@
 
 echo "===== Install kubectl on Ubuntu 22.04 LTS ====="
 
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin/
-kubectl version -o yaml
 
 
