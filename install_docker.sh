ab3882371e9b4a1f473bbace585570693dac5ec2
CDE Test <cde_test@example.com>
2022-11-27 22:32:14 +0700

    Update config.yml

--- install_docker.sh
+++ install_docker.sh
@@ -4,7 +4,7 @@ echo "===== Install docker on Ubuntu 22.04 LTS ====="
 
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 apt install docker-ce -y
 systemctl status docker
